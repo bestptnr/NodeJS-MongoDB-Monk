8158378a817d24e9e7e317f9e8606b99aa3bcc19
bestptnr <drakking1370@gmail.com>
2022-09-13 18:36:33 +0700

    last

--- routes/movies.js
+++ routes/movies.js
@@ -15,17 +15,46 @@ router.get("/", function (req, res, next) {
   });
 });
 
-router.get("/insert", (req, res) => {
-  CTMovie.insert([{ a: 1, b: 2, c: 3 }], (err, docs) => {
-    console.log(docs);
+router.post("/insert", (req, res) => {
+  CTMovie.insert({
+    nameThai: req.body.nameThai,
+    nameEng: req.body.nameEng,
+    RunningTime: req.body.RunningTime,
+  }).then((data) => {
+    console.log(data);
   });
+  res.redirect("/movies");
 });
 
-router.get("/update", (req, res) => {
-  CTMovie.findOneAndUpdate({ a: "3" }, { $set: { a: "4" } }).then(
-    (updatedDoc) => {
-      res.redirect('../movies')
+router.get("/update/:id", (req, res) => {
+  const _id = req.params.id;
+  CTMovie.findOne({_id:_id},(err,docs)=>{
+    res.render("update",{data:docs})
+    console.log(docs)
+  })
+
+});
+
+router.post("/update/:id",(req,res)=>{
+    const _id = req.params.id;
+    CTMovie.findOneAndUpdate(
+    { _id: _id },
+    {
+      $set: {
+        nameThai: req.body.nameThai,
+        nameEng: req.body.nameEng,
+        RunningTime: req.body.RunningTime,
+      },
     }
-  );
+  ).then((updatedDoc) => {
+    console.log(updatedDoc)
+    res.redirect("../../movies");
+  });
+})
+router.get("/delete/:id", (req, res) => {
+  CTMovie.remove({ _id: req.params.id }).then((data) => {
+    console.log("Delete");
+    res.redirect("/movies");
+  });
 });
 module.exports = router;
